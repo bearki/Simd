deb77f16b258da29f7b813c9913de474e4952a62
bearki <bearki@sixelp.com>
2025-08-29 14:23:14 +0800

    优化cmake

--- prj/cmake/arm.cmake
+++ prj/cmake/arm.cmake
@@ -1,22 +1,30 @@
-if((CMAKE_CXX_COMPILER_ID MATCHES "GNU") AND (NOT(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "7.0.0")))
-	set(COMMON_CXX_FLAGS "${COMMON_CXX_FLAGS} -Wno-psabi")
+# ------------------------
+# ARM 特有编译选项
+# ------------------------
+if((CMAKE_CXX_COMPILER_ID MATCHES "GNU") AND (NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS "7.0.0"))
+    set(COMMON_CXX_FLAGS "${COMMON_CXX_FLAGS} -Wno-psabi")
 endif()
 
+# ARM 32-bit NEON 支持
 if(CMAKE_SYSTEM_PROCESSOR MATCHES "arm" AND NOT CMAKE_SYSTEM_PROCESSOR MATCHES "arm64")
-    if( NOT ((CMAKE_CXX_COMPILER_ID STREQUAL "Clang") OR (CMAKE_CXX_COMPILER MATCHES "clang") OR (CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")))
-	  set(CXX_NEON_FLAG "-mfpu=neon -mfpu=neon-fp16")
-	endif()
-	if(CMAKE_CXX_COMPILER_ID MATCHES "GNU")
-		set(CXX_NEON_FLAG "${CXX_NEON_FLAG} -mfp16-format=ieee")
-	endif()
+    if(NOT (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" OR CMAKE_CXX_COMPILER MATCHES "clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang"))
+        set(CXX_NEON_FLAG "-mfpu=neon -mfpu=neon-fp16")
+    endif()
+    if(CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+        set(CXX_NEON_FLAG "${CXX_NEON_FLAG} -mfp16-format=ieee")
+    endif()
 else()
-	set(CXX_NEON_FLAG "")
+    set(CXX_NEON_FLAG "")
 endif()
 
+# Clang 不支持部分 NEON FP16
 if((CMAKE_CXX_COMPILER_ID STREQUAL "Clang") OR (CMAKE_CXX_COMPILER MATCHES "clang")  OR (CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang"))
-	add_definitions(-DSIMD_NEON_FP16_DISABLE)
+    add_definitions(-DSIMD_NEON_FP16_DISABLE)
 endif()
 
+# ------------------------
+# 设置源文件编译选项
+# ------------------------
 file(GLOB_RECURSE SIMD_BASE_SRC ${SIMD_ROOT}/src/Simd/SimdBase*.cpp)
 set_source_files_properties(${SIMD_BASE_SRC} PROPERTIES COMPILE_FLAGS "${COMMON_CXX_FLAGS}")
 
@@ -25,21 +33,40 @@ set_source_files_properties(${SIMD_NEON_SRC} PROPERTIES COMPILE_FLAGS "${COMMON_
 
 file(GLOB_RECURSE SIMD_LIB_SRC ${SIMD_ROOT}/src/Simd/SimdLib.cpp)
 set_source_files_properties(${SIMD_LIB_SRC} PROPERTIES COMPILE_FLAGS "${COMMON_CXX_FLAGS} ${CXX_NEON_FLAG}")
+
+# ------------------------
+# 创建 Simd 库
+# ------------------------
 add_library(Simd ${SIMD_LIB_TYPE} ${SIMD_LIB_SRC} ${SIMD_BASE_SRC} ${SIMD_NEON_SRC})
 
+# ------------------------
+# 测试代码
+# ------------------------
 if(SIMD_TEST)
-	file(GLOB_RECURSE TEST_SRC_C ${SIMD_ROOT}/src/Test/*.c)
-	file(GLOB_RECURSE TEST_SRC_CPP ${SIMD_ROOT}/src/Test/*.cpp)
-	if((NOT ${SIMD_TARGET} STREQUAL "") OR (CMAKE_CXX_COMPILER_VERSION VERSION_LESS "5.0.0"))
-		set_source_files_properties(${TEST_SRC_CPP} PROPERTIES COMPILE_FLAGS "${COMMON_CXX_FLAGS} ${CXX_NEON_FLAG} -D_GLIBCXX_USE_NANOSLEEP")
-	else()
-		set_source_files_properties(${TEST_SRC_CPP} PROPERTIES COMPILE_FLAGS "${COMMON_CXX_FLAGS} ${SIMD_TEST_FLAGS} -mtune=native -D_GLIBCXX_USE_NANOSLEEP")
-	endif()
-	add_executable(Test ${TEST_SRC_C} ${TEST_SRC_CPP})
-	target_link_libraries(Test Simd -lpthread -lstdc++ -lm)
-	if(SIMD_OPENCV)
-		target_compile_definitions(Test PUBLIC SIMD_OPENCV_ENABLE)
-		target_link_libraries(Test ${OpenCV_LIBS})
-		target_include_directories(Test PUBLIC ${OpenCV_INCLUDE_DIRS})
-	endif()
+    file(GLOB_RECURSE TEST_SRC_C ${SIMD_ROOT}/src/Test/*.c)
+    file(GLOB_RECURSE TEST_SRC_CPP ${SIMD_ROOT}/src/Test/*.cpp)
+
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+        # MSVC 编译测试
+        set_source_files_properties(${TEST_SRC_CPP} PROPERTIES COMPILE_FLAGS "${COMMON_CXX_FLAGS}")
+    elseif((NOT "${SIMD_TARGET}" STREQUAL "") OR (CMAKE_CXX_COMPILER_VERSION VERSION_LESS "5.0.0"))
+        set_source_files_properties(${TEST_SRC_CPP} PROPERTIES COMPILE_FLAGS "${COMMON_CXX_FLAGS} ${CXX_NEON_FLAG} -D_GLIBCXX_USE_NANOSLEEP")
+    else()
+        set_source_files_properties(${TEST_SRC_CPP} PROPERTIES COMPILE_FLAGS "${COMMON_CXX_FLAGS} ${SIMD_TEST_FLAGS} -mtune=native -D_GLIBCXX_USE_NANOSLEEP")
+    endif()
+
+    add_executable(Test ${TEST_SRC_C} ${TEST_SRC_CPP})
+    target_link_libraries(Test Simd)
+
+    if(NOT MSVC)
+        target_link_libraries(Test -lpthread -lstdc++ -lm)
+    endif()
+
+    # OpenCV 支持
+    if(SIMD_OPENCV)
+        find_package(OpenCV REQUIRED)
+        target_compile_definitions(Test PUBLIC SIMD_OPENCV_ENABLE)
+        target_link_libraries(Test ${OpenCV_LIBS})
+        target_include_directories(Test PUBLIC ${OpenCV_INCLUDE_DIRS})
+    endif()
 endif()
--- prj/cmake/x86.cmake
+++ prj/cmake/x86.cmake
@@ -4,12 +4,21 @@ elseif(CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64")
 	set(COMMON_CXX_FLAGS "${COMMON_CXX_FLAGS} -m64")
 endif()
 
+# ------------------------
+# 基础 SIMD 源文件
+# ------------------------
 file(GLOB_RECURSE SIMD_BASE_SRC ${SIMD_ROOT}/src/Simd/SimdBase*.cpp)
 set_source_files_properties(${SIMD_BASE_SRC} PROPERTIES COMPILE_FLAGS "${COMMON_CXX_FLAGS}")
 
+# ------------------------
+# SSE4.1 源文件
+# ------------------------
 file(GLOB_RECURSE SIMD_SSE41_SRC ${SIMD_ROOT}/src/Simd/SimdSse41*.cpp)
 set_source_files_properties(${SIMD_SSE41_SRC} PROPERTIES COMPILE_FLAGS "${COMMON_CXX_FLAGS} -msse -msse2 -msse3 -mssse3 -msse4.1 -msse4.2")
 
+# ------------------------
+# AVX2 源文件
+# ------------------------
 file(GLOB_RECURSE SIMD_AVX2_SRC ${SIMD_ROOT}/src/Simd/SimdAvx2*.cpp)
 if ((CMAKE_CXX_COMPILER MATCHES "clang") OR (CMAKE_CXX_COMPILER_ID MATCHES "Clang"))
 	set_source_files_properties(${SIMD_AVX2_SRC} PROPERTIES COMPILE_FLAGS "${COMMON_CXX_FLAGS} -mavx -mavx2 -mfma -mf16c -mbmi -mbmi2 -mlzcnt")
@@ -17,69 +26,95 @@ else()
 	set_source_files_properties(${SIMD_AVX2_SRC} PROPERTIES COMPILE_FLAGS "${COMMON_CXX_FLAGS} -mavx -mavx2 -mfma -mf16c -mbmi -mbmi2 -mlzcnt -mno-avx256-split-unaligned-load -mno-avx256-split-unaligned-store")
 endif()
 
+# ------------------------
+# SIMD 库总 FLAGS
+# ------------------------
 set(SIMD_LIB_FLAGS "${COMMON_CXX_FLAGS} -mavx2 -mfma")
+# ------------------------
+# 汇总算法源文件
+# ------------------------
 set(SIMD_ALG_SRC ${SIMD_BASE_SRC} ${SIMD_SSE41_SRC} ${SIMD_AVX1_SRC} ${SIMD_AVX2_SRC})
 
-if((((CMAKE_CXX_COMPILER_ID MATCHES "GNU") OR (CMAKE_CXX_COMPILER MATCHES "gnu")) AND (NOT(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "5.5.0"))) OR (CMAKE_CXX_COMPILER MATCHES "clang") OR (CMAKE_CXX_COMPILER_ID MATCHES "Clang"))    
-	file(GLOB_RECURSE SIMD_AVX512BW_SRC ${SIMD_ROOT}/src/Simd/SimdAvx512bw*.cpp)
-	set_source_files_properties(${SIMD_AVX512BW_SRC} PROPERTIES COMPILE_FLAGS "${COMMON_CXX_FLAGS} -mavx512f -mavx512cd -mavx512bw -mavx512vl -mavx512dq -mbmi -mbmi2 -mlzcnt -mfma -mf16c")
+# ------------------------
+# AVX-512 / AMX 源文件 (只 GCC/Clang)
+# ------------------------
+if(NOT MSVC)
+    # AVX-512BW
+    if((((CMAKE_CXX_COMPILER_ID MATCHES "GNU") OR (CMAKE_CXX_COMPILER MATCHES "gnu")) AND (NOT(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "5.5.0"))) OR (CMAKE_CXX_COMPILER MATCHES "clang") OR (CMAKE_CXX_COMPILER_ID MATCHES "Clang"))
+        file(GLOB_RECURSE SIMD_AVX512BW_SRC ${SIMD_ROOT}/src/Simd/SimdAvx512bw*.cpp)
+        set_source_files_properties(${SIMD_AVX512BW_SRC} PROPERTIES COMPILE_FLAGS "${COMMON_CXX_FLAGS} -mavx512f -mavx512cd -mavx512bw -mavx512vl -mavx512dq -mbmi -mbmi2 -mlzcnt -mfma -mf16c")
 
-	if(UNIX AND SIMD_AVX512)
-		set(SIMD_LIB_FLAGS "${SIMD_LIB_FLAGS} -mavx512bw")
-		set(SIMD_ALG_SRC ${SIMD_ALG_SRC} ${SIMD_AVX512F_SRC} ${SIMD_AVX512BW_SRC})
-		if(SIMD_INFO)
-			message("Use AVX-512BW")
-		endif()
-	else()
-		add_definitions(-DSIMD_AVX512BW_DISABLE -DSIMD_AVX512VNNI_DISABLE -DSIMD_AMXBF16_DISABLE)
-	endif()
-else()
-	add_definitions(-DSIMD_AVX512BW_DISABLE -DSIMD_AVX512VNNI_DISABLE -DSIMD_AMXBF16_DISABLE)
-endif()
+        if(UNIX AND SIMD_AVX512)
+            set(SIMD_LIB_FLAGS "${SIMD_LIB_FLAGS} -mavx512bw")
+            set(SIMD_ALG_SRC ${SIMD_ALG_SRC} ${SIMD_AVX512F_SRC} ${SIMD_AVX512BW_SRC})
+            if(SIMD_INFO)
+                message("Use AVX-512BW")
+            endif()
+        else()
+            add_definitions(-DSIMD_AVX512BW_DISABLE -DSIMD_AVX512VNNI_DISABLE -DSIMD_AMXBF16_DISABLE)
+        endif()
+    else()
+        add_definitions(-DSIMD_AVX512BW_DISABLE -DSIMD_AVX512VNNI_DISABLE -DSIMD_AMXBF16_DISABLE)
+    endif()
 
-if(((CMAKE_CXX_COMPILER_ID MATCHES "GNU") OR (CMAKE_CXX_COMPILER MATCHES "gnu")) AND (NOT(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "8.0.0")))    
-	file(GLOB_RECURSE SIMD_AVX512VNNI_SRC ${SIMD_ROOT}/src/Simd/SimdAvx512vnni*.cpp)
-	set_source_files_properties(${SIMD_AVX512VNNI_SRC} PROPERTIES COMPILE_FLAGS "${COMMON_CXX_FLAGS} -mavx512f -mavx512cd -mavx512bw -mavx512vl -mavx512dq -mavx512vnni")
+    # AVX-512VNNI
+    if(((CMAKE_CXX_COMPILER_ID MATCHES "GNU") OR (CMAKE_CXX_COMPILER MATCHES "gnu")) AND (NOT(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "8.0.0")))    
+        file(GLOB_RECURSE SIMD_AVX512VNNI_SRC ${SIMD_ROOT}/src/Simd/SimdAvx512vnni*.cpp)
+        set_source_files_properties(${SIMD_AVX512VNNI_SRC} PROPERTIES COMPILE_FLAGS "${COMMON_CXX_FLAGS} -mavx512f -mavx512cd -mavx512bw -mavx512vl -mavx512dq -mavx512vnni")
 
-	if(UNIX AND SIMD_AVX512VNNI)
-		set(SIMD_LIB_FLAGS "${SIMD_LIB_FLAGS} -mavx512vnni")
-		set(SIMD_ALG_SRC ${SIMD_ALG_SRC} ${SIMD_AVX512VNNI_SRC})
-		if(SIMD_INFO)
-			message("Use AVX-512VNNI")
-		endif()
-	else()
-		add_definitions(-DSIMD_AVX512VNNI_DISABLE -DSIMD_AMXBF16_DISABLE)
-	endif()
-else()
-	add_definitions(-DSIMD_AVX512VNNI_DISABLE -DSIMD_AMXBF16_DISABLE)
-endif()
+        if(UNIX AND SIMD_AVX512VNNI)
+            set(SIMD_LIB_FLAGS "${SIMD_LIB_FLAGS} -mavx512vnni")
+            set(SIMD_ALG_SRC ${SIMD_ALG_SRC} ${SIMD_AVX512VNNI_SRC})
+            if(SIMD_INFO)
+                message("Use AVX-512VNNI")
+            endif()
+        else()
+            add_definitions(-DSIMD_AVX512VNNI_DISABLE -DSIMD_AMXBF16_DISABLE)
+        endif()
+    else()
+        add_definitions(-DSIMD_AVX512VNNI_DISABLE -DSIMD_AMXBF16_DISABLE)
+    endif()
 
-if(((CMAKE_CXX_COMPILER_ID MATCHES "GNU") OR (CMAKE_CXX_COMPILER MATCHES "gnu")) AND (NOT(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "11.0.0")))    
-	file(GLOB_RECURSE SIMD_AMXBF16_SRC ${SIMD_ROOT}/src/Simd/SimdAmxBf16*.cpp)
-	set_source_files_properties(${SIMD_AMXBF16_SRC} PROPERTIES COMPILE_FLAGS "${COMMON_CXX_FLAGS} -mavx512f -mavx512cd -mavx512bw -mavx512vl -mavx512dq -mavx512vnni -mavx512vbmi -mavx512bf16 -mamx-tile -mamx-int8 -mamx-bf16")
+    # AMX-BF16
+    if(((CMAKE_CXX_COMPILER_ID MATCHES "GNU") OR (CMAKE_CXX_COMPILER MATCHES "gnu")) AND (NOT(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "11.0.0")))    
+        file(GLOB_RECURSE SIMD_AMXBF16_SRC ${SIMD_ROOT}/src/Simd/SimdAmxBf16*.cpp)
+        set_source_files_properties(${SIMD_AMXBF16_SRC} PROPERTIES COMPILE_FLAGS "${COMMON_CXX_FLAGS} -mavx512f -mavx512cd -mavx512bw -mavx512vl -mavx512dq -mavx512vnni -mavx512vbmi -mavx512bf16 -mamx-tile -mamx-int8 -mamx-bf16")
 
-	if(UNIX AND ((SIMD_AMXBF16 AND (NOT(BINUTILS_VERSION VERSION_LESS "2.34"))) OR SIMD_AMX_EMULATE))
-		set(SIMD_LIB_FLAGS "${SIMD_LIB_FLAGS} -mamx-tile -mamx-int8 -mamx-bf16 -mavx512bf16 -mavx512vbmi")
-		set(SIMD_ALG_SRC ${SIMD_ALG_SRC} ${SIMD_AMXBF16_SRC})
-		if(SIMD_INFO)
-			message("Use AMX-INT8 and AMX-BF16")
-		endif()
-	else()
-		add_definitions(-DSIMD_AMXBF16_DISABLE)
-	endif()
-else()
-	add_definitions(-DSIMD_AMXBF16_DISABLE)
-endif()
+        if(UNIX AND ((SIMD_AMXBF16 AND (NOT(BINUTILS_VERSION VERSION_LESS "2.34"))) OR SIMD_AMX_EMULATE))
+            set(SIMD_LIB_FLAGS "${SIMD_LIB_FLAGS} -mamx-tile -mamx-int8 -mamx-bf16 -mavx512bf16 -mavx512vbmi")
+            set(SIMD_ALG_SRC ${SIMD_ALG_SRC} ${SIMD_AMXBF16_SRC})
+            if(SIMD_INFO)
+                message("Use AMX-INT8 and AMX-BF16")
+            endif()
+        else()
+            add_definitions(-DSIMD_AMXBF16_DISABLE)
+        endif()
+    else()
+        add_definitions(-DSIMD_AMXBF16_DISABLE)
+    endif()
+endif() # end MSVC check
 
+
+# ------------------------
+# SimdLib.cpp
+# ------------------------
 file(GLOB_RECURSE SIMD_LIB_SRC ${SIMD_ROOT}/src/Simd/SimdLib.cpp)
 set_source_files_properties(${SIMD_LIB_SRC} PROPERTIES COMPILE_FLAGS "${SIMD_LIB_FLAGS}")
+
+# ------------------------
+# 添加库
+# ------------------------
 add_library(Simd ${SIMD_LIB_TYPE} ${SIMD_LIB_SRC} ${SIMD_ALG_SRC})
+# MSVC 设置 Runtime Library
 if(MSVC AND SIMD_LIB_TYPE STREQUAL "STATIC")
     set_target_properties(Simd PROPERTIES
         MSVC_RUNTIME_LIBRARY ${CMAKE_MSVC_RUNTIME_LIBRARY}
     )
 endif()
 
+# ------------------------
+# 测试框架
+# ------------------------
 if(SIMD_TEST)
 	file(GLOB_RECURSE TEST_SRC_C ${SIMD_ROOT}/src/Test/*.c)
 	if((CMAKE_CXX_COMPILER MATCHES "clang") OR (CMAKE_CXX_COMPILER_ID MATCHES "Clang")) 
